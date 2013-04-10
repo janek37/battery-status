822e4ee2d7f1951bee1e6a7c5ea9d0717449cea5
Jan Szejko <j-sz@o2.pl>
2013-04-10 13:31:39 +0200

    battery module is now based on acpi command

--- battery.py
+++ battery.py
@@ -1,56 +1,28 @@
 #!/usr/bin/env python3
 import sys
 from time import sleep
+import subprocess
+import re
 
-BATTERY_STATUS_FILE = "/proc/acpi/battery/BAT0/state"
-BATTERY_INFO_FILE = "/proc/acpi/battery/BAT0/info"
-
-def parse_file(input_file, keys):
-  res = {}
-  for line in input_file:
-    key = line.split(":")[0]
-    data = line.split(":")[1].strip().split(" ")[0]
-    if key in keys:
-      res[key] = data
-  return tuple(res[key] for key in keys)
-
-def battery_capacity():
-  capacity, = parse_file(open(BATTERY_INFO_FILE), ('last full capacity',))
-  return int(capacity)
+ACPI_CMD = 'acpi'
 
 def battery_state():
-  rate, remaining, state = parse_file(open(BATTERY_STATUS_FILE),
-    ('present rate', 'remaining capacity', 'charging state'))
-  try:
-    rate = int(rate)
-  except ValueError:
-    rate = 0
-  remaining =  int(remaining)
-  if state != "discharging" or not rate:
-    return state, remaining, None
+  info_text = subprocess.check_output(
+    ACPI_CMD).split('\n')[0].split(':', 1)[1].strip()
+  parts = [part.strip() for part in info_text.split(',')]
+  state = parts[0].lower()
+  percent = float(parts[1].strip('%'))
+  if len(parts) > 2:
+    hours, minutes = (int(part) for part in parts[2].split(':', 2)[:2])
   else:
-    return state, remaining, rate
+    hours = minutes = None
+  return state, percent, hours, minutes
 
 def state():
   return battery_state()[0]
 
-def time_left():
-  state, remaining, rate = battery_state()
-  if rate:
-    return float(remaining) / rate
+def percent_full():
+  return battery_state()[1]
 
 def time_left_hm():
-  return hours_minutes(time_left())
-
-def hours_minutes(hours):
-  if hours:
-    full_hours = int(hours)
-    minutes = int((hours - full_hours) * 60)
-    return full_hours, minutes
-  else:
-    return 0, 0
-
-def percent_full():
-  state, remaining, rate = battery_state()
-  capacity = battery_capacity()
-  return remaining * 100. / capacity
+  return battery_state()[2:]
